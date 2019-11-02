f674f88801a46e87f099976d90fd8aa5552ae4b3
timothyleong97 <timothy.leong@u.nus.edu>
2019-11-02 17:01:12 +0800

    Create Cheatsheet.m
    initial commit

---
+++ Cheatsheet.m
@@ -0,0 +1,47 @@
+% Lab 1
+    % Basic Operations
+        % + - * / for arithmetic
+        % ^ for exponentiation
+        % format rat, long for rationals or floating point decimals
+    % Elementary Functions
+        % log (1.23) + sin (4.5 e6) + acos (0.78) + tanh (910)
+        % 4.5e6 means 4.5 * 10^6
+        % sqrt, nthroot 
+    % Function Handles
+        % f = @(x) x^2 + x + 1
+        % Use semicolons to suppress output
+        % g = @(x) x > 5; logicals -> 1 for true, 0 for false
+        % Piecewise functions: h = @(x) g(x) * x^2 + (1 - g(x)) * (-x ^2);
+        % Or, h = @(x) (x > 5) * x^2 + (x <= 5) * (-x ^2);
+        % For more intervals, pf = @(x) (x > 5) * x^2 + 
+        %       (x <= 5 & x > 3) * x + (x <= 3) * (-x ^2);
+    % Plotting the functions
+        % f = @(x) sin(x) + log(x);
+        % ezplot(f);
+        % e.g. To use the gamma function in ezplot, we need to create a 
+        % function handle for gamma, i.e. ezplot ( @gamma );
+        % Changing the range of the variable: ezplot ( @gamma , [3 ,5]);
+% Lab 2
+% Lab 3
+% Lab 4
+% Lab 5
+
+% Tut 1
+% Tut 2
+% Tut 3
+% Tut 4
+
+% Lecture 1
+% Lecture 2
+% Lecture 3
+% Lecture 4
+% Lecture 5
+% Lecture 6
+% Lecture 7
+% Lecture 8
+% Lecture 9
+% Lecture 10
+
+%Midterm test
+
+%Documentation links
